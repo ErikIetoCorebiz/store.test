8e57cccc06a5ef13d6be86a0040abc6905ace291
Erik Ieto <82223277+ErikIetoCorebiz@users.noreply.github.com>
2021-04-16 11:25:28 -0300

    Initial commit

---
+++ src/@vtex/gatsby-theme-store/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { Fragment, lazy } from 'react'
+import type { FC } from 'react'
+import { SuspenseViewport } from '@vtex/store-ui'
+
+import Toast from '../../../components/Toast'
+import Header from './Header'
+
+const loader = () => import('./Footer')
+
+const Footer = lazy(loader)
+
+const Layout: FC = ({ children }) => (
+  <Fragment>
+    <Header />
+    <Toast />
+    {children}
+    <SuspenseViewport fallback={null} preloader={loader}>
+      <Footer />
+    </SuspenseViewport>
+  </Fragment>
+)
+
+export default Layout
